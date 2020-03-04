09c0bd7197ffbcb974559b010b5aeb7a694d5795
Mina Malaj <mina.malaj@hotmail.com>
2020-03-04 16:30:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,14 +2,14 @@ def using_push(array, string)
   array.push(string)
 end
 
-def using_push(array, string)
-  array.push(string)
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
-def using_push(array)
+def using_pop(array)
   array.pop()
 end
 
-def using_push(array)
+def using_pop(array)
   array.pop[2]
 end
